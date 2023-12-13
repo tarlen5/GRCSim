a5194a4b2f113ae90e0d0a9b162284e3c0d3909c
Weidong Jin <wjin4@crimson.ua.edu>
2023-12-13 15:50:19 -0800

    Change the script, it now take a 8x1 vector as a input. Eeach element is appended to the corresponding dateset.

--- dummyHDF5.cpp
+++ dummyHDF5.cpp
@@ -1,47 +1,68 @@
 #include <highfive/highfive.hpp>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace HighFive;
 
+bool fileExist(const std::string& filename) {
+    std::ifstream file(filename.c_str());
+    return file.good();
+}
+
+ // Define dataset names
+std::vector<std::string> datasetNames = {"egyPrim", "egySec", "theta", "phi", "time", "thetap", "xi", "weight"};
+
+
 void To_HDF5(const std::string& filename,
-             const std::string& groupName,
-             const std::string& datasetName,
-             const std::vector<std::vector<double>>& data,
-             const std::string& xAxis,
-             const std::string& yAxis) {
+             const std::vector<std::vector<double>>& data) {
     try {
-        // Create a new HDF5 file for writing
-        File file(filename, HighFive::File::ReadWrite | HighFive::File::Create);
 
-        // Create the specified group
-        auto group = file.createGroup(groupName);
-
-        // Create the specified dataset within the group
-        DataSet dataset = group.createDataSet(datasetName, data);
-
-        // Create XAxis and YAxis attributes for the dataset
-        dataset.createAttribute("XAxis", std::string(xAxis));
-        dataset.createAttribute("YAxis", std::string(yAxis));
+           // Create or open the specified group
+           if (fileExist(filename))
+                 {
+                  File file(filename, HighFive::File::ReadWrite);
+                  for (size_t i = 0; i < datasetNames.size(); ++i)  {
+                  DataSet dataset = file.getDataSet(datasetNames[i]);
+                  size_t currentSize = dataset.getDimensions()[1];
+                  dataset.resize({1,currentSize+1});
+                  dataset.select({0,currentSize},{1,1}).write(data[i]); }
+                 }
+           else  {
+                             // Create a new file if it doesn't exist
+                  File file(filename, HighFive::File::ReadWrite | HighFive::File::Create);
+                   DataSpace dataspace = DataSpace({1, 1}, {100, DataSpace::UNLIMITED});
+                   DataSetCreateProps props;
+                   props.add(Chunking(std::vector<hsize_t>{2, 2}));
+                  for (size_t i = 0; i < datasetNames.size(); ++i) {
+                  DataSet dataset = file.createDataSet(datasetNames[i],dataspace,  create_datatype<double>(),props);
+                  dataset.select({0, 0}, {1, 1}).write(data[i]);}
+                 }
+         
 
         // Print a message indicating the dataset and group name
-        std::cout << "Dataset '" << datasetName << "' saved to " << filename << " under group '" << groupName << "'" << std::endl;
+        std::cout << "Data appended to " << filename  << "'" << std::endl;
 
     } catch (const HighFive::Exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        // You may choose to throw an exception or handle the error in some other way
-    }
+        std::cerr << "Error: " << e.what() << std::endl;}
+        // You may choose to throw an exception or handle the error in some other way 
 }
 
 int main() {
     // Example usage
-    std::vector<std::vector<double>> egyPrimData = {
-        {1e6, 2e6, 3e6},
-        {100, 150, 300}
+    std::vector<std::vector<double>> Data = {
+        {3.57e6},
+        {5e5},
+        {55.74},
+        {0.57},
+        {1.4e4},
+        {0.6},
+        {0.778},
+        {0.34}
     };
-
-    To_HDF5("histograms.h5", "egyPrim", "Histogram", egyPrimData, "egyPrim", "Num");
-
+    To_HDF5("histograms.h5", Data);
 
     return 0;
 }
+
